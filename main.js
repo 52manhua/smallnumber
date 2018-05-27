55c984d9370e2bcf639e5ddd3bff4e0f14e50f59
52manhua <top355@yopmail.com>
2018-05-28 06:50:45 +0800

    tbl

--- main.js
+++ main.js
@@ -6,12 +6,24 @@
 
 for(var i=1;i<=199;i++){
     $("#layout").append("<span style='margin: 3px' width='30px'><button id='b"+i+"'>" + i + "</button></span>");
-    if(i % 10 ==0 )$("#layout").append("<br>");
+    if(i % 10 == 0 )$("#layout").append("<br>");
     $("#b" + i).bind("click",function(){
         alert(this.innerHTML);
     });
 }
 
+$("#layout").append("<table id='tbl' class='table table-striped'></table>");
+
+var r = 0;
+for(var i=1 ;i<=199 ; i++){
+    if(i % 10 == 1){
+        $("#tbl").append("<tr id='tr"+ r +"'></tr>");
+        r++;
+    }
+    //tr0 时 
+    $("#tr"+ (r-1)).append("<td>" + i +"</td>");
+    //if(i % 10 == 0)$("#tbl").append("</tr>");
+}
 //3、将按钮转变成 div 排布
 //4、制作小兰布的动画，作为背景，数字作为前景
 //5、增加添加或减少的按钮，固定在页面底端
